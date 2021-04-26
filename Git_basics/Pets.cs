d4cecbe7bafec0201351c2bf883b60299fd70aaf
Kristen Harrell <71024265+kristen-harrell@users.noreply.github.com>
2021-04-26 13:48:43 -0400

    fixed spacing in pets class

--- Git_basics/Pets.cs
+++ Git_basics/Pets.cs
@@ -6,8 +6,5 @@ namespace Git_basics
 {
     class Pets
     {
-        public string FirstName { get; set; }
-        public int AnimalId { get; set; }
-        public string PetSpecies { get; set; }
-    }
+        
 }
