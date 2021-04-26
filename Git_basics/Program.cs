34eff2d2bc9cabd8d1653e596d2d80d5b5dc8a5a
Kristen Harrell <71024265+kristen-harrell@users.noreply.github.com>
2021-04-26 14:20:40 -0400

    first commit on new branch

--- Git_basics/Program.cs
+++ Git_basics/Program.cs
@@ -6,9 +6,7 @@ namespace Git_basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and Welcome to my project!");
-            Console.WriteLine("This project is for testing git commands");
-            Console.WriteLine("So glad you are here");
+            Console.WriteLine("Let's fix this page up");
         }
     }
 }
